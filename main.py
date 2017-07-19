b0df9b1f6a98e4eaf39f3fd59947e393576de532
Will Yelton <willyelton@steveny-macbook.local>
2017-07-19 13:51:37 -0400

    added main class

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame as pg
+
+class main():
+    def __init__(self):
+        self.windowWidth = 500
+        self.windowHeight = 500
+
+    def run(self):
+        print ("runing")
+
+def runMain():
+    mainClass = main()
+    mainClass.run()
+
+runMain()
